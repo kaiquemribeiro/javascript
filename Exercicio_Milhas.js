a2b503d58d36bfa87e1cbeaf912f47f8f57420bf
kaiquemribeiro <kaiqueribeiro@alunos.utfpr.edu.br>
2020-03-27 20:09:14 -0300

    JS

--- Exercicio_Milhas.js
+++
@@ -1,8 +0,0 @@
-const METROS = 0.3048, KILOMETROS = 1.60934
-let milhas, pes
-
-milhas = prompt("\nDigite o valor em milhas\n");
-pes = prompt("\nDigite o valor em pes\n");
-
-console.log("O valor em kilometros e " + (milhas * KILOMETROS));
-console.log("O valor em pes e " + (pes * METROS));
