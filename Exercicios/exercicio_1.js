34ff7e966d7e741a2d9ecc5c057e88eaa30d28a9
kaiquemribeiro <kaiqueribeiro@alunos.utfpr.edu.br>
2020-03-30 19:27:19 -0300

    JS

---
+++ Exercicios/exercicio_1.js
@@ -0,0 +1,9 @@
+/* Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração,
+multiplicação e divisão desses valores.*/
+
+function operacao(numero1, numero2){
+    console.log(numero1 + numero2);
+    console.log(numero1 - numero2);
+    console.log(numero1 / numero2);
+}
+operacao(1, 2);
---
+++ Exercicios/exercicio_3.js
@@ -0,0 +1,6 @@
+/*Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.*/
+
+function calculaExpoente(base, expoente){
+    return base ** expoente;
+}  
+console.log(calculaExpoente(4, 6));
