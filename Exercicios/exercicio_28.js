251578c5e121a777e460800049b3ac79e60f8065
kaiquemribeiro <kaiqueribeiro@alunos.utfpr.edu.br>
2020-03-31 01:59:59 -0300

    JS

---
+++ Exercicios/exercicio_28.js
@@ -0,0 +1,13 @@
+/*Ler um vetor de números inteiros e imprimir quantos são pares e quantos são ímpares.*/
+
+let vetor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+let p = 0;
+let x = 0;
+
+for (i = 0; i <= 20; i++){
+    if (vetor[i] % 2 === 0) p++;
+    if (vetor[i] % 2 != 0) x++
+}
+
+console.log(`${p} Numeros par`);
+console.log(`${x} Numeros impar`);
